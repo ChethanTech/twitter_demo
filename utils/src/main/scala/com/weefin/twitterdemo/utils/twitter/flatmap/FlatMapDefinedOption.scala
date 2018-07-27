0f2d0c967796fbc65891c01620fedb14614aeedb
Julien ALEXANDRE <22328659+ndrpnt@users.noreply.github.com>
2018-07-27 10:18:27 +0200

    Improve FlatMap function to handle nested options

--- utils/src/main/scala/com/weefin/twitterdemo/utils/twitter/flatmap/FlatMapDefinedOption.scala
+++ utils/src/main/scala/com/weefin/twitterdemo/utils/twitter/flatmap/FlatMapDefinedOption.scala
@@ -3,11 +3,20 @@ package com.weefin.twitterdemo.utils.twitter.flatmap
 import org.apache.flink.api.common.functions.RichFlatMapFunction
 import org.apache.flink.util.Collector
 
-class FlatMapDefinedOption[T] extends RichFlatMapFunction[Option[T], T] {
-	override def flatMap(value: Option[T], out: Collector[T]): Unit = value.foreach(out.collect(_))
+import scala.annotation.tailrec
+
+class FlatMapDefinedOption[T, U] extends RichFlatMapFunction[T, U] {
+	override def flatMap(value: T, out: Collector[U]): Unit = {
+		@tailrec def go(value: Any): Unit = value match {
+			case o: Option[Any] if o.isDefined => go(o.get)
+			case u: U => out.collect(u)
+		}
+		
+		go(value)
+	}
 }
 
 object FlatMapDefinedOption {
-	def apply[T] = new FlatMapDefinedOption[T]
+	def apply[T, U] = new FlatMapDefinedOption[T, U]
 }
 
