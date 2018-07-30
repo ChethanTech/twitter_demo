521c6e844cc71fc75b7dcbfcfe509896e7ffad3d
Julien ALEXANDRE <22328659+ndrpnt@users.noreply.github.com>
2018-07-30 12:03:52 +0200

    Move KeyedProcessThrottle to utils for modularity

--- processing_examples/extract_users/src/main/scala/com/weefin/twitterdemo/ExtractUsers.scala
+++ processing_examples/extract_users/src/main/scala/com/weefin/twitterdemo/ExtractUsers.scala
@@ -3,11 +3,10 @@ package com.weefin.twitterdemo
 import com.danielasfregola.twitter4s.entities.Tweet
 import com.typesafe.scalalogging.LazyLogging
 import com.weefin.twitterdemo.utils.twitter.entities.{SimpleStatus, SimpleUser}
+import com.weefin.twitterdemo.utils.twitter.process.KeyedProcessThrottle
 import com.weefin.twitterdemo.utils.twitter.sink.KafkaJsonProducer
 import com.weefin.twitterdemo.utils.twitter.source.KafkaJsonConsumer
 import org.apache.flink.api.common.functions.RichFlatMapFunction
-import org.apache.flink.api.common.state.{ValueState, ValueStateDescriptor}
-import org.apache.flink.streaming.api.functions.KeyedProcessFunction
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.util.Collector
@@ -20,28 +19,12 @@ object ExtractUsers extends App with LazyLogging {
 	env.addSource(consumer).flatMap(FlatMapSimpleUser).keyBy(_.id).process(FilterDuplicates).addSink(producer)
 	env.execute
 	
-	private object FilterDuplicates extends KeyedProcessFunction[Long, SimpleUser, SimpleUser] {
-		private lazy val state: ValueState[Boolean] = getRuntimeContext
-			.getState(new ValueStateDescriptor[Boolean]("throttle", classOf[Boolean]))
-		
-		override def onTimer(timestamp: Long,
-			ctx: KeyedProcessFunction[Long, SimpleUser, SimpleUser]#OnTimerContext,
-			out: Collector[SimpleUser]): Unit = state.clear()
-		
-		override def processElement(value: SimpleUser,
-			ctx: KeyedProcessFunction[Long, SimpleUser, SimpleUser]#Context,
-			out: Collector[SimpleUser]): Unit = if (!state.value) {
-			out.collect(value)
-			state.update(true)
-			ctx.timerService
-				.registerEventTimeTimer(ctx.timerService.currentProcessingTime() + Time.minutes(10).toMilliseconds)
-		}
-	}
-	
 	private object FlatMapSimpleUser extends RichFlatMapFunction[Option[Tweet], SimpleUser] {
 		override def flatMap(value: Option[Tweet], out: Collector[SimpleUser]): Unit = value
 			.foreach(SimpleStatus(_).user.foreach(out.collect(_)))
 	}
+	
+	private def FilterDuplicates = KeyedProcessThrottle[Long, SimpleUser, SimpleUser](Time.minutes(10), x => x)
 	
 	private def consumer = KafkaJsonConsumer[Tweet](params.consumerBootstrapServers,
 		params.consumerTopicId,
---
+++ utils/src/main/scala/com/weefin/twitterdemo/utils/twitter/process/KeyedProcessThrottle.scala
@@ -0,0 +1,28 @@
+package com.weefin.twitterdemo.utils.twitter.process
+
+import org.apache.flink.api.common.state.{ValueState, ValueStateDescriptor}
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.util.Collector
+
+class KeyedProcessThrottle[K, I, O](throttle: Time, f: I => O) extends KeyedProcessFunction[K, I, O] {
+	private lazy val state: ValueState[Boolean] = getRuntimeContext
+		.getState(new ValueStateDescriptor[Boolean]("throttle", classOf[Boolean]))
+	
+	override def onTimer(timestamp: Long,
+		ctx: KeyedProcessFunction[K, I, O]#OnTimerContext,
+		out: Collector[O]): Unit = state.clear()
+	
+	override def processElement(value: I,
+		ctx: KeyedProcessFunction[K, I, O]#Context,
+		out: Collector[O]): Unit = if (!state.value) {
+		out.collect(f(value))
+		state.update(true)
+		ctx.timerService.registerProcessingTimeTimer(ctx.timerService.currentProcessingTime() + throttle
+			.toMilliseconds)
+	}
+}
+
+object KeyedProcessThrottle {
+	def apply[K, I, O](throttle: Time, f: I => O) = new KeyedProcessThrottle[K, I, O](throttle, f)
+}
