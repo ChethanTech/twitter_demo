6efdf692c4eecd8d0a9a1778b898bf46453d14a3
Julien ALEXANDRE <22328659+ndrpnt@users.noreply.github.com>
2018-07-30 09:00:54 +0200

    Clean up pattern matching

--- utils/src/main/scala/com/weefin/twitterdemo/utils/twitter/flatmap/FlatMapDefinedOption.scala
+++ utils/src/main/scala/com/weefin/twitterdemo/utils/twitter/flatmap/FlatMapDefinedOption.scala
@@ -8,8 +8,10 @@ import scala.annotation.tailrec
 class FlatMapDefinedOption[T, U] extends RichFlatMapFunction[T, U] {
 	override def flatMap(value: T, out: Collector[U]): Unit = {
 		@tailrec def go(value: Any): Unit = value match {
-			case o: Option[Any] if o.isDefined => go(o.get)
+			case o: Some[Any] => go(o.get)
+			case None =>
 			case u: U => out.collect(u)
+			case _ => throw new IllegalArgumentException("Invalid types: T must wrap U in an arbitrary number of Options")
 		}
 		
 		go(value)
