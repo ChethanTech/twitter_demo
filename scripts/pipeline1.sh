7e444fd6c2eb1e16a4fd6031263de69762541abe
Julien ALEXANDRE <22328659+ndrpnt@users.noreply.github.com>
2018-08-27 08:26:02 +0200

    Reverse jobs launch order

--- scripts/pipeline1.sh
+++ scripts/pipeline1.sh
@@ -5,6 +5,14 @@ source pipeline1.cfg
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd "$platformPath"
 
+./flink-1.5.2/bin/flink run -d "$dir/../processing_examples/classify_tweets/target/classify_tweets-0.1.jar" \
+    --consumer.bootstrap.servers "localhost:9092" \
+    --producer.bootstrap.servers "localhost:9092" \
+    --consumer.group.id "g1" \
+    --consumer.topic.id "pipeline1_raw_tweets" \
+    --producer.topic.id "pipeline1_rich_tweets" \
+    --classification-file "$classificationFile"
+
 ./flink-1.5.2/bin/flink run -d "$dir/../twitter_stream_to_kafka/target/twitter_stream_to_kafka-0.1.jar" \
     --uri "/1.1/statuses/filter.json?track=$trackedKeywords" \
     --http-method "GET" \
@@ -14,12 +22,4 @@ cd "$platformPath"
     --twitter-source.tokenSecret "$tokenSecret" \
     --bootstrap.servers "localhost:9092" \
     --topic.id "pipeline1_raw_tweets"
-
-./flink-1.5.2/bin/flink run -d "$dir/../processing_examples/classify_tweets/target/classify_tweets-0.1.jar" \
-    --consumer.bootstrap.servers "localhost:9092" \
-    --producer.bootstrap.servers "localhost:9092" \
-    --consumer.group.id "g1" \
-    --consumer.topic.id "pipeline1_raw_tweets" \
-    --producer.topic.id "pipeline1_rich_tweets" \
-    --classification-file "$classificationFile"
 
--- scripts/pipeline2.sh
+++ scripts/pipeline2.sh
@@ -5,6 +5,14 @@ source pipeline2.cfg
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd "$platformPath"
 
+./flink-1.5.2/bin/flink run -d "$dir/../processing_examples/classify_tweets/target/classify_tweets-0.1.jar" \
+    --consumer.bootstrap.servers "localhost:9092" \
+    --producer.bootstrap.servers "localhost:9092" \
+    --consumer.group.id "g1" \
+    --consumer.topic.id "pipeline2_raw_tweets" \
+    --producer.topic.id "pipeline2_rich_tweets" \
+    --classification-file "$classificationFile"
+
 ./flink-1.5.2/bin/flink run -d "$dir/../twitter_user_timelines_to_kafka/target/twitter_user_timelines_to_kafka-0.1.jar" \
     --user-ids "$userIds" \
     --twitter-source.consumerKey "$consumerKey" \
@@ -15,12 +23,3 @@ cd "$platformPath"
     --topic.id "pipeline2_raw_tweets" \
     --tweet-count "$tweetCount" \
     --query-count "$queryCount"
-
-./flink-1.5.2/bin/flink run -d "$dir/../processing_examples/classify_tweets/target/classify_tweets-0.1.jar" \
-    --consumer.bootstrap.servers "localhost:9092" \
-    --producer.bootstrap.servers "localhost:9092" \
-    --consumer.group.id "g1" \
-    --consumer.topic.id "pipeline2_raw_tweets" \
-    --producer.topic.id "pipeline2_rich_tweets" \
-    --classification-file "$classificationFile"
-
